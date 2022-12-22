0a0f877db2a5e6c96244fe0f6e53c440200490f2
gallit0 <mateogesede@gmail.com>
2022-12-21 22:54:45 -0300

    task 1 done

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,2 +1,13 @@
 #!/usr/bin/node
-
+const h = document.getElementById('amenities_selected')
+var checked = {}
+var inputs = Array.prototype.slice.call(document.getElementsByTagName("input"))
+inputs.forEach(input => {
+  input.addEventListener("click", function(check){
+    const element = check['srcElement']
+    const data = element['dataset']
+    if (checked[data['id']]) delete checked[data['id']]
+    else checked[data['id']] = data['name']
+    h.innerText = Object.values(checked).join(', ')
+  });
+})
